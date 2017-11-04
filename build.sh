b3bdb51210cdbcfcadf0cdc3a167ef7d35aeceb7
Florent Gallaire <fgallaire@gmail.com>
2017-11-04 18:12:14 +0000

    Remove source files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/txt2tags/txt2tags/080e3143eea50c8a77eac03b87723858aa3a0035/txt2tags
-chmod 755 txt2tags
-
-for file in */*.t2t; do
-    ./txt2tags $file
-done
