aa3988a606ac80014242d8a7ea86d41349acc3b7
Florent Gallaire <fgallaire@gmail.com>
2017-11-04 05:11:37 +0000

    Rules build

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 wget https://raw.githubusercontent.com/txt2tags/txt2tags/080e3143eea50c8a77eac03b87723858aa3a0035/txt2tags
 chmod 755 txt2tags
 
+for testfile in comment.t2t image.t2t inline.t2t line.t2t link.t2t list.t2t paragraph.t2t quote.t2t table.t2t test.conf title.t2t verbatim.t2t; do
+    wget -P English/rules https://raw.githubusercontent.com/txt2tags/txt2tags/master/test/marks/$testfile
+done
+
+wget -P English/rules https://raw.githubusercontent.com/txt2tags/txt2tags/master/test/macro/syntax.t2t
+
+sleep 10
+
 for file in */*.t2t; do
     ./txt2tags $file
 done
